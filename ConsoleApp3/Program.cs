a55903bc1caeda98159113d6973797dcfb9f648b
John Battle <v-johnbattle@microsoft.com>
2022-03-17 11:00:37 -0400

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Text;
+
+
+
+string myURL = "https://api.tvmaze.com/search/shows?q=Glee";
+
+HttpWebRequest HttpWReq = 
+    (HttpWebRequest)WebRequest.Create(myURL);
+
+
+HttpWebResponse HttpWResp = (HttpWebResponse)HttpWReq.GetResponse();
+Stream receiveStream = HttpWResp.GetResponseStream();
+
+
+Encoding encode = Encoding.GetEncoding("utf-8");
+
+
+
+StreamReader readStream = new StreamReader(receiveStream, encode);
+Char[] read = new Char[256];
+
+int count = readStream.Read( read, 0, 256);
+
+while (count > 0)
+{
+    String str = new String(read, 0, count);
+    Console.Write(str);
+    count = readStream.Read(read, 0, 256);
+}
+
+Console.WriteLine("");
+
+readStream.Close();
+
+HttpWResp.Close();
+
+
+
